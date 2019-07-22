f6c5ced234cd546cbbf277b44c808157754e6396
chenhg <chggyf@163.com>
2019-07-22 09:28:47 +0800

    cd

--- lht_01_test/src/com/fx/Test.java
+++ lht_01_test/src/com/fx/Test.java
@@ -1,7 +1,7 @@
 package com.fx;
 
 public class Test {
-public static void main(String[] args) {
-	System.out.println("");
-}
+	public static void main(String[] args) {
+		System.out.println("");
+	}
 }
