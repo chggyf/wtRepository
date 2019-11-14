84a84c9f359e1825898905edd73c6e2a50cc969c
chggyf <chggyf@163.com>
2019-11-15 06:46:22 +0800

    ssdf

--- lht_01_test/src/com/fx/Ytdf.java
+++ lht_01_test/src/com/fx/Ytdf.java
@@ -5,4 +5,8 @@ package com.fx;
  * @Date: 2019/11/15 * @Description: com.fx * @version: 1.0
  */
 public class Ytdf {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("");
+    }
 }
